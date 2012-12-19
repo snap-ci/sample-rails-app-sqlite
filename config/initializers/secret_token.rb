34ddb64257478dc88695d06bfd3446e719c418e1
Sahil Muthoo <sahil.muthoo@gmail.com>
2012-12-19 11:37:42 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRailsAppSqlite::Application.config.secret_token = 'd5bb78b6c88435892e8614e6b5b486c1b690e7f176255cefb2a10d4a2707a82eae4a6695558052acd8531cab33a12158c4f9bcc6ff689d3d09b5cddb0b155089'
